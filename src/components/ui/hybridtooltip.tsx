93a02719c754d90a5560ff8c649f323c824e64fe
Will Heath <williamtheath@icloud.com>
2025-10-07 22:05:47 -0700

    feat(shadcn): added shadcn components

---
+++ src/components/ui/hybridtooltip.tsx
@@ -0,0 +1,36 @@
+import { type PropsWithChildren, createContext, useContext, useEffect, useState } from 'react';
+import { Tooltip, TooltipTrigger, TooltipContent } from './tooltip';
+import { Popover, PopoverTrigger, PopoverContent } from './popover';
+import type { TooltipContentProps, TooltipProps, TooltipTriggerProps } from '@radix-ui/react-tooltip';
+import type { PopoverContentProps, PopoverProps, PopoverTriggerProps } from '@radix-ui/react-popover';
+
+const TouchContext = createContext<boolean | undefined>(undefined);
+const useTouch = () => useContext(TouchContext);
+
+export const TouchProvider = (props: PropsWithChildren) => {
+    const [isTouch, setTouch] = useState<boolean>();
+
+    useEffect(() => {
+        setTouch(window.matchMedia('(pointer: coarse)').matches);
+    }, []);
+
+    return <TouchContext.Provider value={isTouch} {...props} />;
+};
+
+export const HybridTooltip = (props: TooltipProps & PopoverProps) => {
+    const isTouch = useTouch();
+
+    return isTouch ? <Popover {...props} /> : <Tooltip {...props} />;
+};
+
+export const HybridTooltipTrigger = (props: TooltipTriggerProps & PopoverTriggerProps) => {
+    const isTouch = useTouch();
+
+    return isTouch ? <PopoverTrigger {...props} /> : <TooltipTrigger {...props} />;
+};
+
+export const HybridTooltipContent = (props: TooltipContentProps & PopoverContentProps) => {
+    const isTouch = useTouch();
+
+    return isTouch ? <PopoverContent {...props} /> : <TooltipContent {...props} />;
+};
