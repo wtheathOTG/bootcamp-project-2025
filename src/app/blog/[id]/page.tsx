31939d9ea7a96271da700099ae9ae656d3f53ace
Will Heath <williamtheath@icloud.com>
2025-10-09 16:12:47 -0700

    feat: title metaData for async dynamic blog routes

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import {blogs} from "@/blog";
 
+export async function generateMetadata({ params }: { params: Promise<{id: string}> }) {
+    const {id} = await params;
+    const blog = blogs.find(blog => blog.slug === id);
+
+    return {
+        title: blog ? blog.title : "Blog Not Found",
+    };
+}
+
 async function Page({ params }: { params: Promise<{id: string}> }) {
     const {id} = await params;
     const blog = blogs.find(blog => blog.slug === id);
 
+    //TODO date, image
     return (
         <div className="text-foreground">
             <h2 className="pb-8 text-3xl capitalize">{blog?.title}</h2>
