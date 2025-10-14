03f5d46a51bdf795fd26bfcbee590caa427ff622
Will Heath <williamtheath@icloud.com>
2025-10-13 20:45:48 -0700

    fix: Removed .next and .idea folders from repository and update .gitignore

--- .next/static/chunks/src_app_blog_page_tsx_fff7e528._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_1d960ddb._.js"
-],
-    source: "dynamic"
-});
